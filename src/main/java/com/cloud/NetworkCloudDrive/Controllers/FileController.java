f3973effca92e3fe5ae75fe28ed2650fc6d0cf5e
Aksellercan <akselercan2003@gmail.com>
2025-10-22 08:24:14 +0200

    add: download and its endpoint #4 #6

--- src/main/java/com/cloud/NetworkCloudDrive/Controllers/FileController.java
+++ src/main/java/com/cloud/NetworkCloudDrive/Controllers/FileController.java
@@ -5,14 +5,15 @@ import com.cloud.NetworkCloudDrive.Models.JSONResponse;
 import com.cloud.NetworkCloudDrive.Services.IOService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-
-import java.util.Map;
 
 @RestController
 @RequestMapping(path = "/api/file")
@@ -28,12 +29,26 @@ public class FileController {
     public ResponseEntity<?> UploadFile(@RequestParam MultipartFile file) {
         try {
             logger.info("filename before upload: {}", file.getOriginalFilename());
-            FileMetadata fileUpload = ioService.UploadImage(file);
+            FileMetadata fileUpload = ioService.UploadFile(file);
             return ResponseEntity.ok().body(fileUpload);
         } catch (Exception e) {
             logger.error("File controller, {}", e.getMessage());
         }
         return ResponseEntity.badRequest().body(new JSONResponse("Failed to upload file", "api/file/upload", false));
+    }
+
+    @PostMapping("download")
+    public ResponseEntity<?> DownloadFile(@RequestParam long fileId) {
+        try {
+            FileMetadata metadata = ioService.GetFileMetadata(fileId);
+            Resource file = ioService.DownloadFile(fileId);
+            return ResponseEntity.ok().header(
+                    HttpHeaders.CONTENT_DISPOSITION,
+                    "attachment; filename=\"" + metadata.getMimiType() + "\" ")
+                    .contentType(MediaType.parseMediaType(metadata.getMimiType())).contentLength(metadata.getSize()).body(file);
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().body(new JSONResponse("Failed to download file", false));
+        }
     }
 
 }
--- src/main/java/com/cloud/NetworkCloudDrive/Services/IOService.java
+++ src/main/java/com/cloud/NetworkCloudDrive/Services/IOService.java
@@ -2,11 +2,14 @@ package com.cloud.NetworkCloudDrive.Services;
 
 import com.cloud.NetworkCloudDrive.Models.FileMetadata;
 import com.cloud.NetworkCloudDrive.Repositories.SQLiteFileRepository;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Optional;
 
 @Service
 public class IOService {
@@ -19,12 +22,24 @@ public class IOService {
     }
 
 
-    public FileMetadata UploadImage(MultipartFile file) throws IOException {
+    public FileMetadata UploadFile(MultipartFile file) throws IOException {
         String storagePath;
         try (InputStream inputStream = file.getInputStream()) {
             storagePath = fileService.StoreFile(inputStream, file.getOriginalFilename());
         }
         FileMetadata metadata = new FileMetadata(file.getOriginalFilename(), storagePath, file.getContentType(), file.getSize());
         return sqLiteFileRepository.save(metadata);
+    }
+
+    public Resource DownloadFile(long fileId) throws Exception{
+        Optional<FileMetadata> file = sqLiteFileRepository.findById(fileId);
+        if (file.isEmpty()) throw new FileNotFoundException(String.format("%d\n", fileId));
+        return fileService.RetrieveFile(file.get().getPath());
+    }
+
+    public FileMetadata GetFileMetadata(long fileId) throws Exception{
+        Optional<FileMetadata> file = sqLiteFileRepository.findById(fileId);
+        if (file.isEmpty()) throw new FileNotFoundException(String.format("%d\n", fileId));
+        return file.get();
     }
 }
